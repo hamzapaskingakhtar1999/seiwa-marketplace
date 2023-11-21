64a37b05dfd54036e6ffca2781a1da85aa7eff56
Hamza Pasking Akhtar <hamzapaskingakhtar@gmail.com>
2023-11-21 16:43:36 +0500

    Complete sign up functionality

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -7,7 +7,8 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json("User succesfully Createdd");
+    console.log(newUser);
+    res.status(201).json("User succesfully Created");
   } catch (error) {
     next(error);
   }
