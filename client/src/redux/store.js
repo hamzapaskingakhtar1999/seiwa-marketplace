29728a0b8b8284b13180041225420fc5cf2a819a
Hamza Pasking Akhtar <hamzapaskingakhtar@gmail.com>
2023-11-23 10:50:42 +0500

    Added Redux Persist to save state to local storage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,33 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
+
+import storage from "redux-persist/lib/storage";
+
+import { persistReducer, persistStore } from "redux-persist";
 
 import userReducer from "./user/userSlice";
 
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
